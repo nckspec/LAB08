2de28cd5f5402671a48fa32ea505ce94c851c104
Macbook <macbook@Macbooks-MacBook-Air.local>
2019-01-23 08:37:44 -0800

    Second commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,60 @@
+/*********************************************************
+ *  AUTHOR          : Nicholas Spector & Victor Truong
+ *  Lab #4          : Loops
+ *  CLASS           : CS2
+ *  SECTION         : MTWR: 07:30a – 12:30p
+ *  Due Date        : 01/23/19
+ *********************************************************/
+
+
 #include <iostream>
 
 using namespace std;
 
+
+/*******************************************************************************
+ *
+ * LAB08
+ *_________________________________________________________
+ *
+ *
+ *
+ ******************************************************************************/
+
+
+
 int main()
 {
-    cout << "Hello World!" << endl;
+
+
+    // variable
+    int ex;              //INPUT - The exercise that the user is choosing
+
+
+    // INPUT - User is asked to type in the exercise number
+    cout << "Which exercise: \n";
+    ex = 0;
+    cin >> ex;
+
+
+    // PROCESSING - Chooses exercise 1 if "1" is chosen
+    if(ex == 1)
+    {
+
+
+    }
+
+    // PROCESSING: Chooses exercise 2 if '2' is chosen
+    if(ex == 2)
+    {
+
+
+    }
+
+
     return 0;
+
 }
+
+
+
