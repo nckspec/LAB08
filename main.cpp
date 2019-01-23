5fc0d7b824ebfa3946f289bf3b9ba6460993c5fc
Victor Truong <me@ifvictr.com>
2019-01-23 08:58:11 -0800

    Add exercise 1

--- main.cpp
+++ main.cpp
@@ -7,7 +7,10 @@
  *********************************************************/
 
 
+#include <cstdlib>
 #include <iostream>
+
+int SumDigits(int);
 
 using namespace std;
 
@@ -28,11 +31,11 @@ int main()
 
 
     // variable
-    int ex;              //INPUT - The exercise that the user is choosing
-
+    int ex;              //INPUT - The exercd "/home/victor/projects/LAB08/" && g++ main.cpp -o main && "/home/victor/projects/LAB08/"maincise that the user is choosing
+    int num;
 
     // INPUT - User is asked to type in the exercise number
-    cout << "Which exercise: \n";
+    cout << "Which exercise: ";
     ex = 0;
     cin >> ex;
 
@@ -40,8 +43,19 @@ int main()
     // PROCESSING - Chooses exercise 1 if "1" is chosen
     if(ex == 1)
     {
-
-
+        do
+        {
+            cout << "Please enter an integer (0 to quit): ";
+            cin >> num;
+            if (num != 0)
+            {
+                cout << "The sum of the digits of " << num << " is " << SumDigits(num) << "\n";
+            }
+            else
+            {
+                cout << "Goodbye\n";
+            }
+        } while (num != 0);
     }
 
     // PROCESSING: Chooses exercise 2 if '2' is chosen
@@ -58,3 +72,20 @@ int main()
 
 
 
+int SumDigits(int num)
+{
+    int sum;
+    int currDigit;
+    sum = 0;
+    currDigit = 0;
+    num = abs(num);
+
+    while (num > 0)
+    {
+        currDigit = num % 10;
+        sum += currDigit;
+        num /= 10;
+    }
+
+    return sum;
+}
